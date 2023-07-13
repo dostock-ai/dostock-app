b358d30d533464a9c05705475e49e020da31a63f
PedroCandia <petercandiavazquez@hotmail.com>
2023-07-13 17:50:37 -0600

    [dev] Nueva estructura pages [dev] Delete folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,9 +8,14 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'sales',
+    loadChildren: () => import('./pages/sales/sales.module').then( m => m.SalesPageModule)
+  },
+  {
+    path: 'orders',
+    loadChildren: () => import('./pages/orders/orders.module').then( m => m.OrdersPageModule)
   }
+
 ];
 
 @NgModule({

df1ea6ab138449b2124552775d5b79717246c86d
PedroCandia <petercandiavazquez@hotmail.com>
2023-10-12 13:51:52 -0600

    [dev] environments keys

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    supabase: {
+      auth: {
+        supabaseURL: 'https://ocawxswurzuxadztfqtw.supabase.co',
+        supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jYXd4c3d1cnp1eGFkenRmcXR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkzNzE4MDQsImV4cCI6MjAwNDk0NzgwNH0.5q2_92UGd47nnA012PpWjEuhOOf_CzufYfLbP3josEc',
+      }
+    }
+  };
---
+++ src/environments/environtment.ts
@@ -0,0 +1,13 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    supabase: {
+      auth: {
+        supabaseURL: 'https://ocawxswurzuxadztfqtw.supabase.co',
+        supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jYXd4c3d1cnp1eGFkenRmcXR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkzNzE4MDQsImV4cCI6MjAwNDk0NzgwNH0.5q2_92UGd47nnA012PpWjEuhOOf_CzufYfLbP3josEc',
+      }
+    }
+  };

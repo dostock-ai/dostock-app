50e200199e0cb27bc10b22aeccd6ab9fdff24de5
PedroCandia <petercandiavazquez@hotmail.com>
2023-11-06 11:06:43 -0600

    [dev] Eliminamos las credenciales [dev] Diseño al Chat AI Tory

--- src/app/pages/menu/chat-ai/chat-ai.page.ts
+++ src/app/pages/menu/chat-ai/chat-ai.page.ts
@@ -48,7 +48,8 @@ export class ChatAIPage implements OnInit{
   }
 
   async getMessageChatGPT(msg: any) {
-    const resChatGPT = await this.chatgptSvc.chatgpt(msg);
-    this.messages.push({ sender: 'chat-ai-message', text: resChatGPT });
+    // const resChatGPT = await this.chatgptSvc.chatgpt(msg);
+    // this.messages.push({ sender: 'chat-ai-message', text: resChatGPT });
+    this.messages.push({ sender: 'chat-ai-message', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' });
   }
 }
